00299cc04265d40052531999b14cbb6d56ec9ca3
Tanner <troxs690@gmail.com>
2022-09-20 10:53:58 -0500

    commit tree d029d5b79f9b4691b6f95ad66b2b1eabbe526349

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+def badge_maker (name)
+  "Hello, my name is #{name}."
+end
+
+
+def batch_badge_creator (names)
+  names.map do |name|
+   
+      "Hello, my name is #{name}."
+       
+    
+    end
+end
